0dcd243c82ce59afef435895f41011d96c7e64ff
Wybe <wybe@ruurdwestra.nl>
2019-12-29 11:43:29 +0100

    + Add start and condition node.

---
+++ ui/ProgramGraphEdit.gd
@@ -0,0 +1,17 @@
+extends GraphEdit
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_connection_request(from, from_slot, to, to_slot):
+	self.connect_node(from, from_slot, to, to_slot)
